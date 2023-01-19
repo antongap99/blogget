de2213f2484ec499f9a2567da68c673fbb64393d
Anton.gap <anton.gap99@gmail.com>
2023-01-19 17:04:36 +0300

    deploy

--- src/api/const.js
+++ src/api/const.js
@@ -2,7 +2,7 @@ export const URL_AUTH = 'https://www.reddit.com/api/v1/authorize?';
 export const CLIENT_ID = 'lBFnXTgJbw99_3_Ebc7KUg';
 export const RESPONSE_TYPE = 'token';
 export const RANDOM_STRING = 'random_string';
-export const REDIRECT_URI = 'http://localhost:3000/auth';
+export const REDIRECT_URI = 'https://blogget-opal.vercel.app/auth';
 export const SCOPE_STRING = 'identity read submit'; // область видимости (права доступа к различной инфе аккаунта пользователя)
 export const URL_API = 'https://oauth.reddit.com';
 
